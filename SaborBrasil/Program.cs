95fb2af150cd616ba7c2620aafaaf1ae5310d08d
Victor <victorgit28@outlook.com>
2025-05-07 09:43:55 -0300

    Controler

--- SaborBrasil/Program.cs
+++ SaborBrasil/Program.cs
@@ -1,10 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using SaborBrasilContext;
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySQL("server=localhost;database=saborDeMinas;user=root;password=1234"));
-
-app.MapGet("/", () => "Hello World!");
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+app.UseStaticFiles();
+app.MapGet("/", () => Results.Redirect("/index.cshtml"));
 
 app.Run();
