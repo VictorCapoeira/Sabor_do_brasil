58a0b0d777870ed3556abba00c69a40f059b2396
Victor <victorgit28@outlook.com>
2025-05-07 08:45:09 -0300

    Criado conexão

--- SaborBrasil/Program.cs
+++ SaborBrasil/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using SaborBrasilContext;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySQL("server=localhost;database=saborDeMinas;user=root;password=1234"));
 
 app.MapGet("/", () => "Hello World!");
 
