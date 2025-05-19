57e84eb9a334a6c96210164777eb877075e5c2e4
Victor <victorgit28@outlook.com>
2025-05-19 09:02:38 -0300

    Ajustado css e html da publicação

--- SaborBrasilMvc/wwwroot/js/script.js
+++ SaborBrasilMvc/wwwroot/js/script.js
@@ -38,6 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
         commentBtns.forEach(btn => {
             btn.addEventListener('click', function() {
                 commentsModal.show();
+                // Aqui você pode carregar os comentários da publicação usando btn.getAttribute('data-id')
             });
         });
     }
