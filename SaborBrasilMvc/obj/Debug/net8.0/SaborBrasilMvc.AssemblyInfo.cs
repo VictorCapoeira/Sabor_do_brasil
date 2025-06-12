0a46d0509d5f49e0bb2763bbace832dc9d987a1e
Victor <victorgit28@outlook.com>
2025-06-12 09:02:50 -0300

    teste

--- SaborBrasilMvc/obj/Debug/net8.0/SaborBrasilMvc.AssemblyInfo.cs
+++ SaborBrasilMvc/obj/Debug/net8.0/SaborBrasilMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaborBrasilMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a94e6d895ed315fe7edde9211a4680f14a01f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54ede350d8b9c9bf3a0bf8f139b10787e5025f74")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaborBrasilMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaborBrasilMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
